38bf70bbbcd95839a4a9fb2d1b0ed1685f291af2
felipeprata10 <felipeprata10@gmail.com>
2025-11-18 20:49:06 -0300

    Adicionado IDs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -27,5 +27,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        id_var = findViewById(R.id.etx_id);
+        nome_var = findViewById(R.id.etx_nome);
+        documento_var = findViewById(R.id.etx_documento);
+        curso_var = findViewById(R.id.etx_curso);
     }
 }
