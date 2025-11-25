cfe7c37d45f505b1a872f91b867e46ae7f63b2bf
felipeprata10 <felipeprata10@gmail.com>
2025-11-25 20:34:37 -0300

    Adicionado ids

--- app/src/main/java/com/example/myapplication/LogicaCadastro.java
+++ app/src/main/java/com/example/myapplication/LogicaCadastro.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -11,17 +13,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class LogicaCadastro extends AppCompatActivity {
 
     EditText id_var, nome_var, documento_var, curso_var;
 
     ArrayList<Alunos> alunos_var = new ArrayList<>();
 
+    Button botao_cadastrar, botao_visualizar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tela_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -32,5 +36,12 @@ public class MainActivity extends AppCompatActivity {
         nome_var = findViewById(R.id.etx_nome);
         documento_var = findViewById(R.id.etx_documento);
         curso_var = findViewById(R.id.etx_curso);
+
+        botao_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome_var
+            }
+        });
     }
 }
